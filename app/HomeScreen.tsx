fcaefb3ab1096757e892f349772fae3a59d6ec99
Sohan Chaudhari <169914576+sohanasz@users.noreply.github.com>
2025-12-01 21:30:15 +0530

    Implemented language select screen

---
+++ app/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>OPEN APP</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="home" options={{ title: "Home" }}></Stack.Screen>
+    </Stack>
+  );
 }
